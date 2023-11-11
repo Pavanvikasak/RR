68991b5e04f37448226753181df4894ad3c9992c
pavanvikas ak <akpavanvikas@gmail.com>
2023-11-11 16:41:38 +0530

    v5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,8 +35,8 @@ const RESOURCES = {"assets/AssetManifest.json": "9fde57f5e166bd614d1ad6d7d8e5a7b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "875d8c3dcaf40b262912053dbfbd1b67",
-"/": "875d8c3dcaf40b262912053dbfbd1b67",
+"index.html": "caeae8314c8f834a2fd01cd169164b8b",
+"/": "caeae8314c8f834a2fd01cd169164b8b",
 "main.dart.js": "75ed2b4d5db45736117da7711d96e52a",
 "manifest.json": "dee793c96aaeaa67874f1084f3b7672e",
 "version.json": "ade8cd5081f37b318401d7a58930105a"};
